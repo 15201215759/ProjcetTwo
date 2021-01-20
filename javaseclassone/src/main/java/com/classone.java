52f65e3da587754a09da3edbc4725a87e50341fb
15201215759 <15201215759>
2021-01-20 21:28:21 +0800

    测试git提交

--- javaseclassone/src/main/java/com/classone.java
+++ javaseclassone/src/main/java/com/classone.java
@@ -4,5 +4,6 @@ public class classone {
     public static void main(String[] args) {
         System.out.println("hello word!");
         System.out.println("ce");
+        System.out.println("测试git提交");
     }
 }
