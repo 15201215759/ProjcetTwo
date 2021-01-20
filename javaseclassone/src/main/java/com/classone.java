20327e0622773e8179eee5a416ac433324e0c44d
15201215759 <15201215759>
2021-01-20 22:13:57 +0800

    测试方法

--- javaseclassone/src/main/java/com/classone.java
+++ javaseclassone/src/main/java/com/classone.java
@@ -6,5 +6,17 @@ public class classone {
         System.out.println("ce");
         System.out.println("测试git提交");
         System.out.println("测试git提交2");
+        methodOne();
+        methodTwo();
+        String s = methodTwo();
+        System.out.println(s);
+    }
+    public static  void methodOne(){
+        System.out.println("没有返回值，不传参的方法");
+    }
+    public static String methodTwo(){
+        System.out.println("有返回值的方法，不传参");
+        return "methodTwo的返回值";
     }
 }
+
